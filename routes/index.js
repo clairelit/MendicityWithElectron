ecff963643d0a94263cab730f5b6eca4a0a99527
Claire Murray <clairefromcork@gmail.com>
2016-04-15 22:03:29 +0100

    CCan;t ge search wokinng
    I've tried loads of stuff, but nothing seems to work.  I've moved away from this for now and I'm working on inserting the image into the searchpage.  This is kind of working, in that, I'm getting the image icon (like when an image url isn't loading on a website), but it's closer than I've been all day!

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-
+var path = require('path');
 //var db = require('diskdb');
 //db = db.connect('public/mendoDatabase', ['mendoPeopleList']);
 
@@ -35,20 +35,81 @@ router.get('/', function(req, res, next) {
 router.get('/searchpage', function(req, res, next) {
       //var db = new Datastore({'mendoPeopleList':'public/mendoDatabase/mendoPeopleList.json'});
       mendoPeopleList.find({}, function (err, docs) {
-        console.log(docs);
+       // console.log(docs);
         res.render('searchpage', {allPeople: docs});
     });
 });
 
 
+router.post('/searchpag', function(req, res, next){
+    mendoPeopleList.find({county: { $exists: true}}, function (err, docs){
+        console.log('county');
+        res.render('searchresults');
+    });
+})
+/*router.post('/searchpage', function(req, res, next){
+    //var db = req.db;
+    //var collection = db.get('mendoPeopleList');
+    //db.mendoPeopleList.find({'county.'})
+    //var query = {};
+    if (req.body.county) {
+    console.log(req.body.county);
+    query.county = req.body.county;
+    //collection.find({"county":" "});
+}
+    if (req.body.surname) {
+        console.log(req.body.surname);
+    query.surname = req.body.surname;
+    //collection.find({"surname":" "});
+}
+
+    if (req.body.country) {
+        console.log(req.body.country);
+   query.country = req.body.country;
+   //collection.find({"country":" "});
+}
+    if (req.body.town) {
+        console.log(req.body.town);
+    query.town = req.body.town;
+    //collection.find({"town":" "});
+}
+    console.log(query);
+    //var db = req.db;
+    //var collection = db.get('mendoPeopleList');
+    mendoPeopleList.find({$and: [query]} , function(e,docs){
+      res.render('searchresults', {"allPeople" : docs
+      });
+    });
+});*/
 
 /*router.post('/searchpage', function(req, res, next){
   var county = (req.body.county);
   var country = (req.body.country);
-  var myArray = db.mendoPeopleList.find({County:county , Country:country});
+  var myArray = mendoPeopleList.find({County:county , Country:country});
   //db stuff will go here
- res.render('/searchpage');
+ res.render('/searchresults.jade');
 ({peopleResults: myArray});//here send in results from the search of db
+});*/
+
+/*router.post ('/searchpage', function(req, res, next){
+    mendoPeopleList.find({$or: [{county: 'county'}, {country: 'country'}, {surname: 'surname'}]}, function (err, docs){
+        console.log('county');
+        res.render('searchresults', {allPeople: docs});
+    });
+});*/
+
+/*router.post('/searchpage', function(req, res, next){
+    var queryData ={
+        query:{
+            county:{ $in: 'county'},
+            country:{ $in: 'country'},
+            surname:{ $in: 'surname'}, function(err, docs){
+                console.log('queryData');
+                res.render('searchresults'), {allPeople: docs});
+            }
+        },
+        
+    }
 });*/
 
 router.get('/whatismendicity', function(req, res, next) {
